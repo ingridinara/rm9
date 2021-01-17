b44b3d6fca0669cef30210b9ba98542070235529
ingridinara <ingridinara@me.com>
2021-01-17 22:17:07 +0100

    searchBarComponent

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react'
+import React  from 'react'
+import '../style/searchbar.css'
 
-export default class Searchbar extends Component {
+export default class Searchbar extends React.Component {
+    state = {
+        term: ' '
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            term: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.handleFormSubmit(this.state.term);
+    }
+
     render() {
         return (
-            <div>
-                
+            <div className="searchbar" >
+                <form  className="searchbar__form" onSubmit={this.handleSubmit} >
+                        <div className="searchbar__formlabelbox">
+                            <label className="searchbar__label" htmlFor="video-search">Video Search</label>
+                        </div>
+                        <div>
+                            <input className="searchbar__input" onChange={this.handleChange} 
+                            name='video-search'type="text" value={this.state.term}/>
+                        </div>
+                </form>
             </div>
         )
     }
