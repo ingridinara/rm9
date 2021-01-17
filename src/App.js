a331fe13ba27ba791ba5185e33e56ceda3eef8a1
ingridinara <ingridinara@me.com>
2021-01-17 22:06:11 +0100

    appComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
+import youtube from './apis/youtube';
+import SearchBar from './components/Searchbar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
-export default class App extends Component {
+class App extends React.Component {
+  state = {
+      videos: [],
+      selectedVideo: null
+  }
+
+  handleSubmit = async (termFromSearchBar) => {
+      const response = await youtube.get('/search', {
+          params: {
+              q: termFromSearchBar
+          }
+      })
+      this.setState({
+          videos: response.data.items
+      })
+      console.log("RESPONSE OBJECT: ",response);
+  };
+  
+  handleVideoSelect = (video) => {
+      this.setState({selectedVideo: video})
+  }
+
   render() {
-    return (
-      <div>
-        APP
-      </div>
-    )
+      return (
+          <div >
+              <SearchBar handleFormSubmit={this.handleSubmit}/>
+              <div>
+                    <div >
+                        <VideoDetail video={this.state.selectedVideo}/>
+                    </div>
+                    <div>
+                        <VideoList handleVideoSelect={this.handleVideoSelect} 
+                        videos={this.state.videos}/>
+                    </div>
+              </div>
+          </div>
+      )
   }
 }
 
+export default App;
